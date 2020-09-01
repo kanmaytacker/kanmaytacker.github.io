0c47131f8ccb811bbbb2c9d6bcb50adf5b108116
kanmaytacker <tanmay@locus.sh>
2020-09-01 22:55:59 +0530

    Updates

---
+++ precache-manifest.28fba3731c4bde478bba5620ab227e22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123d1686cf2e07cd791720be5f630d32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "378a83bd29f687c60df8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b0dd2c8e1ee07caeb27",
+    "url": "/static/js/2.ffd1acd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ffd1acd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378a83bd29f687c60df8",
+    "url": "/static/js/main.4f1baf06.chunk.js"
+  },
+  {
+    "revision": "04dbb3ac965af27e649f",
+    "url": "/static/js/runtime-main.b7c6aaad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
